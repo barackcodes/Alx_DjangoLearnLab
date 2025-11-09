980d5322124a62667c0ef26716245249b5b5b5bd
Barrack <barrackdome45@gmail.com>
2025-11-09 23:55:38 +0300

    Clean up project and add relationship_app

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -9,36 +9,20 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-<<<<<<< HEAD
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-=======
 class Book(models.Model):
     title = models.CharField(max_length=200)
->>>>>>> 79f5061 (Add relationship_app with advanced model relationships)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
 
-<<<<<<< HEAD
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-=======
 class Library(models.Model):
     name = models.CharField(max_length=150)
->>>>>>> 79f5061 (Add relationship_app with advanced model relationships)
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 79f5061 (Add relationship_app with advanced model relationships)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
