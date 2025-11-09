d937460997a010e727e18c57cfbd92af6d5341a6
Barrack <barrackdome45@gmail.com>
2025-11-09 23:26:52 +0300

    Add relationship_app with advanced model relationships

--- relationship_app/query_samples.py
+++ relationship_app/query_samples.py
@@ -1,14 +1,25 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
 
-def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()
+    except Author.DoesNotExist:
+        return f"No author found with the name {author_name}"
 
-def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
-    
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return f"No library found with the name {library_name}"
+
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except Library.DoesNotExist:
+        return f"No library found with the name {library_name}"
+    except Librarian.DoesNotExist:
+        return f"No librarian assigned to {library_name}"
