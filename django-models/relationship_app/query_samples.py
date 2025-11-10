31ee82bd2956949b7894ceba2f1f1b2b1b450c88
Barrack <barrackdome45@gmail.com>
2025-11-10 15:45:17 +0300

    Reorganized project structure: moved LibraryProject into django-models

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,16 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
