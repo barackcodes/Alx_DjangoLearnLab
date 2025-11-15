fa5f3923aaba6dc5ddc942d1f9e5705b0e28575a
Barrack <barrackdome45@gmail.com>
2025-11-15 16:27:15 +0300

    Implement full Django model relationship setup, migrations, project configuration, and query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,20 +1,12 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
-django.setup()
-
 from .models import Author, Book, Library, Librarian
 
+author_name = "Author Name"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
-author_name = "Barrack"
-books = Book.objects.filter(author__name=author_name)
-print(books)
+library_name = "Library Name"
+library = Library.objects.get(name=library_name)
+library_books = library.books.all()
 
-
-library_name = "Free Library"
-books_in_library = Library.objects.get(name=library_name).books.all()
-print(books_in_library)
-
-librarian = Librarian.objects.get(library__name=library_name)
-print(librarian)
+library = Library.objects.get(name=library_name)
+librarian = library.librarian
