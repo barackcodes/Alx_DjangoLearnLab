47bd3d2bbfb939b1dcbde56d0dcac4cc095e06cf
Barrack <barrackdome45@gmail.com>
2025-11-15 16:36:36 +0300

    Implement full Django model relationship setup, migrations, project configuration, and query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,4 +9,4 @@ library = Library.objects.get(name=library_name)
 library_books = library.books.all()
 
 library = Library.objects.get(name=library_name)
-librarian = library.librarian
+librarian = Librarian.objects.get(library=library)
