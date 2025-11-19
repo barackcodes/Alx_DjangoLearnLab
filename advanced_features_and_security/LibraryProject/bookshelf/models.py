96b9a37bcf1b797da1ba2002362ba225b9de801d
Barrack <barrackdome45@gmail.com>
2025-11-19 12:05:11 +0300

    CustomUser setup, bookshelf and relationship_app models

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.conf import settings  # Import settings to reference CustomUser
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # Reference CustomUser
+        on_delete=models.CASCADE
+    )
     publication_year = models.IntegerField()
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}"
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.title} by {self.author.username}"
