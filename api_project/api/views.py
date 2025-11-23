c597d22a4081ee42dd8d6419e254f58caf3a3c02
Barrack <barrackdome45@gmail.com>
2025-11-23 22:49:48 +0300

    Added token authentication + permission to Book API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -10,5 +12,10 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
     
-    
+class AdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff
