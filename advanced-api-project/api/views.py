60a1c577546c92d259a2c52b228715713eb7bade
Barrack <barrackdome45@gmail.com>
2025-11-30 15:22:37 +0300

    Improve API querying capabilities with filters and search features

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,36 +1,25 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Book
 from .serializers import BookSerializer
+
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
     filter_backends = [
         DjangoFilterBackend,
         SearchFilter,
-        OrderingFilter
+        OrderingFilter,
     ]
 
+    
+    filterset_fields = ['title', 'author__name', 'publication_year']
 
-    filterset_fields = [
-        'title',
-        'author__name',
-        'publication_year'
-    ]
+    search_fields = ['title', 'author__name']
 
-    search_fields = [
-        'title',
-        'author__name'
-    ]
-
-    ordering_fields = [
-        'title',
-        'publication_year'
-    ]
+    ordering_fields = ['title', 'publication_year']
     ordering = ['publication_year']
-
