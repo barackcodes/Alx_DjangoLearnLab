1c4d34b64caadc9d804333a718b5a96c177880f9
Barrack <barrackdome45@gmail.com>
2025-11-30 15:31:55 +0300

    Enhance Book API with search, filterset, and ordering integrations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,5 @@
-from rest_framework import generics
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import rest_framework
 
 from .models import Book
 from .serializers import BookSerializer
@@ -13,11 +11,10 @@ class BookListView(generics.ListAPIView):
 
     filter_backends = [
         DjangoFilterBackend,
-        SearchFilter,
-        OrderingFilter,
+        filters.SearchFilter,
+        filters.OrderingFilter,
     ]
 
-    
     filterset_fields = ['title', 'author__name', 'publication_year']
 
     search_fields = ['title', 'author__name']
