b2da7edc852140095dc33f394a4b2a392252e5f3
Barrack <barrackdome45@gmail.com>
2025-11-30 16:02:09 +0300

    Implement full CRUD views for Book model with filtering, search, and ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import generics, filters, permissions
 from rest_framework import generics, filters
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
@@ -22,3 +23,33 @@ class BookListView(generics.ListAPIView):
 
     ordering_fields = ['title', 'publication_year']
     ordering = ['publication_year']
+    
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        
+        serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        
+        serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
