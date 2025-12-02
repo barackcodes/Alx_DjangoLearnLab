04848d0f740a4493cdcf28c643c4500655d72666
Barrack <barrackdome45@gmail.com>
2025-12-02 21:25:55 +0300

    Implement full CRUD functionality for blog posts with permissions and templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -11,7 +11,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-created_at']
     paginate_by = 5
 
 
