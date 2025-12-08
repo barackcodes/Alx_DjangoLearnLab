49578062d057e62b31235790a48486aa78b34aab
Barrack <barrackdome45@gmail.com>
2025-12-08 22:18:18 +0300

    setup Django project, add accounts app, implement CustomUser model with followers field, configure DRF token auth, and apply initial migrations

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.URLField(blank=True, null=True)
-    
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField(
         'self',
         symmetrical=False,
