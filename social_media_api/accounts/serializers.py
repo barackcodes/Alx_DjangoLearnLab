abfdfd88d6fb8560818f8f30876ef4959410ed02
Barrack <barrackdome45@gmail.com>
2025-12-08 22:11:05 +0300

    setup project structure, custom user model, and token authentication

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'bio', 'user_profile']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio =validated_data('bio', ''),
+            profile_picture =validated_data.get('profile_picture', '')
+        )
+        Token.objects.create(user=user)
+        return user
+    
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def validtae(self, data):
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+        
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        data['user'] = user
+        return data
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+        
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    UserSerializer
+)
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get(user=user)
+            return Response({
+                "message": "Registration successful.",
+                "token": token.key,
+                "user": UserSerializer(user).data
+                
+            },status=status.HTTP_201_CREATED)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ =Token.objects.get_or_create(user=user)
+            
+            return Response({
+                "message": "Login successful.",
+                "token": token.key,
+                "user": UserSerializer(user).data
+            })
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        return Response(UserSerializer(request.user).datas)
+            
