ac03935710f73d529db248a24b8d429bf50b05e9
Barrack <barrackdome45@gmail.com>
2025-12-08 22:37:58 +0300

    correct user registration serializer and update accounts URL patterns to meet authentication task requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,47 +1,44 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
-
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password', 'bio', 'user_profile']
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
-        
+
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
             password=validated_data['password'],
-            bio =validated_data('bio', ''),
-            profile_picture =validated_data.get('profile_picture', '')
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
         Token.objects.create(user=user)
         return user
-    
-    
+
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
-    
-    def validtae(self, data):
+
+    def validate(self, data):
         user = authenticate(
             username=data['username'],
             password=data['password']
         )
-        
         if not user:
             raise serializers.ValidationError("Invalid credentials")
         data['user'] = user
         return data
-    
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-        
