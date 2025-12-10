3e23c02b7f279dda095b5a95b60c58fed2202409
Barrack <barrackdome45@gmail.com>
2025-12-10 16:16:32 +0300

    Update FeedView in posts/views.py to include required   Post.objects.filter(author__in=following_users).order_by call

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()
-router.register('posts', PostViewSet, basename='posts')
-router.register('comments', CommentViewSet, basename='comments')
+router.register("posts", PostViewSet, basename="posts")
+router.register("comments", CommentViewSet, basename="comments")
 
 app_name = "posts"
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('feed/', FeedView.as_view(), name='feed')
+    path("", include(router.urls)),
+    path("feed/", FeedView.as_view(), name="feed"),
 ]
