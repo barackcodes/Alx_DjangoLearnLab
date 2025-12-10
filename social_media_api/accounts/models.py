5c2221d9a964f72b55df61e482924e1d3b10b6e1
Barrack <barrackdome45@gmail.com>
2025-12-10 09:14:25 +0300

    Implement follow system in user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,15 +3,14 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.URLField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField(
+
+    following = models.ManyToManyField(
         'self',
         symmetrical=False,
-        related_name='following',
+        related_name='followers',
         blank=True
     )
-    
+
     def __str__(self):
         return self.username
-
