c0dbf4dd2cf99a5416439470395329a3670073ea
Barrack <barrackdome45@gmail.com>
2025-12-10 13:00:47 +0300

    Add user follow system and feed endpoint (follow/unfollow, followers/following lists, feed generation)

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,40 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import get_user_model
+
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+User = get_user_model()
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    pagination_class = None
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at']
+
+    def get_queryset(self):
+        user = self.request.user 
+
+        following_qs = user.following.all()
+
+        return (
+            Post.objects.filter(author__in=following_qs)
+            .order_by('-created_at')
+        )
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
